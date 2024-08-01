026ca8a8624113668f29e3945af21dffdd7fa719
KeiraKistensamy <Keirakist0987@gmail.com>
2024-08-01 14:28:57 +0200

    Exercise2

---
+++ index.js
@@ -0,0 +1,9 @@
+import { Person } from './person.js';
+
+const person1 = new Person('Keira', 'Kistensamy', 'keirakistensamy@gmail.com');
+const person2 = new Person('Erin', 'Williams', 'erinwilliams@gmail.com');
+const person3 = new Person('Chelsea', 'Julie', 'chelseajulie@gmail.com');
+
+// console.log(person1);
+// console.log(person2);
+// console.log(person3);
